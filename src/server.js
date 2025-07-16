bb5565a98519dd643702af05f9a1ed84df484b95
RakeshRathod500 <rockyrathod500@gmail.com>
2025-07-16 13:03:29 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:4200',
+  origin: 'http://65.0.76.75',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
